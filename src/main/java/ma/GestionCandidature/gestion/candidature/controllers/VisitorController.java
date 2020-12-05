13178eb572c7fffdd28e425229d1632923c5770a
Abdelwahab <Abdelwahab.elfilali@edu.uiz.ac.ma>
2020-12-05 18:47:30 +0100

    Zedt Les classes f package Services

---
+++ src/main/java/ma/GestionCandidature/gestion/candidature/controllers/VisitorController.java
@@ -0,0 +1,11 @@
+package ma.GestionCandidature.gestion.candidature.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/visitor")
+public class VisitorController {
+
+
+}
---
+++ src/main/java/ma/GestionCandidature/gestion/candidature/services/CandidatServices.java
@@ -0,0 +1,4 @@
+package ma.GestionCandidature.gestion.candidature.services;
+
+public class CandidatServices {
+}
